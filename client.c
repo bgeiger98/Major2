53596d33dff1e4c8bd1627e1d8feb3a33cef3947
micwil27 <37913544+micwil27@users.noreply.github.com>
2018-04-23 15:29:48 -0500

    Update client.c

--- client.c
+++ client.c
@@ -1,1 +1,18 @@
+//group 19
+//Major 2
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+
+int main(){
+  
+  
+ 
+  return 0;
+}
